83a818284bf1cadb57c9f0574018f939e119af2c
Dromediansk <miroslav.pillar@senacor.com>
2022-12-16 17:20:44 +0100

    handling updates

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,13 @@ import {
   getProducts,
   updateProduct,
 } from "./handlers/product";
+import {
+  createUpdate,
+  deleteUpdate,
+  getOneUpdate,
+  getUpdates,
+  updateUpdate,
+} from "./handlers/update";
 import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
@@ -33,23 +40,24 @@ router.delete("/product/:id", deleteProduct);
 /**
  * Update
  */
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
-  body("title").optional,
-  body("body").optional,
-  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
-  body("version").optional,
-  () => {}
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
+  body("version").optional(),
+  updateUpdate
 );
 router.post(
   "/update",
   body("title").exists().isString(),
   body("body").exists().isString(),
-  () => {}
+  body("productId").exists().isString(),
+  createUpdate
 );
-router.delete("/update/:id", () => {});
+router.delete("/update/:id", deleteUpdate);
 
 /**
  * Update Point
